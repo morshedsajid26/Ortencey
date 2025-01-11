a8af4418874fd07bbae070169ba15d7dcf17c373
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2025-01-11 23:58:05 +0600

    footer top part done

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -6,12 +6,17 @@ const Footer = () => {
     <div className="bg-[url('/public/footer.png')] py-20 bg-black">
       <Container>
 
-        <div className="top">
+        <div className="top flex justify-between items-center border-b pb-[60px] border-[#393939]">
           <div className="left text-white">
-            <p>For Digital Marketing Agency</p>
-            <h3>Join Our Agency Community</h3>
+            <p className='text-[16px]'>For Digital Marketing Agency</p>
+            <h3 className='font-Nunito font-bold text-[25px]'>Join Our Agency Community</h3>
           </div>
-          <div className="right"></div>
+
+          <div className="right relative">
+            <input className='py-[27px] pl-[35px] w-[500px] outline-none rounded-full text-[16px] text-[#B1B1B1]  ' type="text" placeholder='Enter your email address' />
+
+            <button className='bg-[#3661FC] text-white text-[18px] py-[15px] px-[33px] rounded-full absolute right-2.5 top-1/2 -translate-y-1/2'>Subscribe Now</button>
+          </div>
         </div>
 
 
