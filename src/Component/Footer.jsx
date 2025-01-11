ee47cd0b7cc20c46ef4448e65f5bcddb3696d11b
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2025-01-11 23:46:55 +0600

    footer running

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Container from '../Layer/Container'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="bg-[url('/public/footer.png')] py-20 bg-black">
+      <Container>
+
+        <div className="top">
+          <div className="left text-white">
+            <p>For Digital Marketing Agency</p>
+            <h3>Join Our Agency Community</h3>
+          </div>
+          <div className="right"></div>
+        </div>
+
+
+
+        <div className="bottom"></div>
+
+      </Container>
+    </div>
   )
 }
 
